7bec1c5a45eb73c73af132e23844d0bf3002142a
finn-dugan <95668173+finn-dugan@users.noreply.github.com>
2022-04-29 11:38:32 -0700

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include "calc.h"
 #include <cstring>
 
-
 //Write functions in this order.  Constructor and destructor will be built as the
 //functions it invokes are written
 
@@ -74,83 +73,68 @@ void Calc::MakeValueTbl()
 
 void Calc::Parse()
 {
-    int len = strlen(inFix);
-    char *temp = new char[len + 1];
-    int inFixIdx = 0;
-    int tmpIdx = 0;
+    char* temp = new char[strlen(inFix)];
+    strcpy(temp, inFix);
+    delete [] inFix;
+    inFix = new char[strlen(temp)];
+    // int alpha = 65;
+    
+    // for(int i = 0; i < strlen(temp); i++)
+    // {
+    //     if(temp[i] == ' ')
+    //     {
+    //         continue;
+    //     }
+        // if(temp[i] == '+' || temp[i] == '-' || temp[i] == '*' || temp[i] == '/')
+        // {
+        //     inFix[i] = temp[i];
+        // }
+        // else if(temp[i] == '(')
+        // {
+        //     inFix[i] = temp[i];
+        //     stk->Peek();
+        //     stk->Push(temp[i]);
+        // }
+        // else if(temp[i] == ')')
+        // {
+        //     inFix[i] = temp[i];
+        //     stk->Peek();
+        //     stk->Pop();
+        // }
+        // else if(temp[i] >=0))
+        // {
+        //     inFix[i] = temp[i];
+        // }
 
-
-    while (inFix[inFixIdx] != '\0')
-    {
-      if (isdigit(inFix[inFixIdx])) // Okay cool its a number
-      {
-        if (temp >= 65 && temp <= (65 + 26))
-      strncat(tmp, &temp, 1);
-        if (temp == '(')
-      stk->Push(temp);
-        if ((temp == '*') || (temp == '/') || (temp == '+') || (temp == '-'))
-      stk->Push(temp);
-        if (temp == ')')
+        int i = 0;
+        while (temp[i] != '/0')
         {
-      while (stk->Peek() != '(')
-      {
-         char temp2 = stk->Peek();
-         strncat(tmp, &temp2, 1);
-         stk->Pop();
-      }
-      stk->Pop();
+            // if operand add to the postfix expression
+            if(inFix[i]>='a' && inFix[i]<='z'|| inFix[i]>='A'&& inFix[i]<='Z')          
+            {
+                postFix += inFix[i];
+                i++;
+            }
+            // if opening bracket then push the stack
+            else if(inFix[i]=='(')
+            {
+                stk->Push(inFix[i]);
+                i++;
+            }
+            // if closing bracket then pop the stack
+            else if(inFix[i]==')')
+            {
+                stk->Pop();
+                i++;
+            }
+            // if operator then pop the stack and add to the postfix expression
+            else if(inFix[i]=='+'||inFix[i]=='-'||inFix[i]=='*'||inFix[i]=='/')
+            {
+                stk->Pop();
+                postFix += inFix[i];
+                i++;
+            }
         }
-      }
-      else // Uh oh it isnt a number
-      {
-         tmp[tmpIdx] = inFix[inFixIdx];
-         inFixIdx++;
-         tmpIdx++;
-      }
-   }
-    delete[] inFix;
-    inFix = tmp;
-    tmp = NULL;
-    // //1. space for inFix expression is dynamically allocated 
-    // char* inFixCopy = new char[strlen(inFix) + 1];
-    // strcpy(inFixCopy, inFix);
-    // //2. inFixCopy is copied to inFix
-    // strcpy(inFix, inFixCopy);
-    // //3. inFixCopy is deleted
-    // delete [] inFixCopy;
-    // //4. inFix is parsed
-    // for(int i = 0; i < strlen(inFix); i++)
-    // {
-    //     if(inFix[i] == '+' || inFix[i] == '-' || inFix[i] == '*' || inFix[i] == '/')
-    //     {
-    //         stk->Push(inFix[i]);
-    //     }
-    //     else if(inFix[i] >= 'A' && inFix[i] <= 'Z')
-    //     {
-    //         stk->Push(inFix[i]);
-    //     }
-    //     else if(inFix[i] >= '0' && inFix[i] <= '9')
-    //     {
-    //         stk->Push(inFix[i]);
-    //     }
-    //     else if(inFix[i] == '(')
-    //     {
-    //         stk->Push(inFix[i]);
-    //     }
-    //     else if(inFix[i] == ')')
-    //     {
-    //         while(stk->Peek() != '(')
-    //         {
-    //             stk->Pop();
-    //         }
-    //         stk->Pop();
-    //     }
-    // }
-    // //5. while the stack is not empty, pop the stack and push the popped value to the output stack
-    // while(!stk->IsEmpty())
-    // {
-    //     stk->Pop();
-    // }
 }
 
 bool Calc::CheckParens()
@@ -189,34 +173,34 @@ void Calc::DisplayInFix()
   post: postFix points to a dynamically declared array holding the postfix version
         of the the input infix expression.
  */
-void Calc::InFixToPostFix()
-{
-    int len = strlen(inFix);
-    char* temp = new char[len + 1];
+// void Calc::InFixToPostFix()
+// {    
+//     int len = strlen(inFix);
+//     char* temp = new char[len + 1];
 
-    for(int i = 0; inFix[i] != '\0'; i++)
-    {
-        char temp = inFix[i];
+//     for(int i = 0; inFix[i] != '\0'; i++)
+//     {
+//         char temp = inFix[i];
     
-    if (temp >= 65 && temp <= (65 + 26))
-      strncat(tmp, &temp, 1);
-    if (temp == '(')
-      stk->Push(temp);
-    if ((temp == '*') || (temp == '/') || (temp == '+') || (temp == '-'))
-      stk->Push(temp);
-    if (temp == ')')
-    {
-      while (stk->Peek() != '(')
-      {
-         char temp2 = stk->Peek();
-         strncat(tmp, &temp2, 1);
-         stk->Pop();
-      }
-      stk->Pop();
-    }
+//     if (temp >= 65 && temp <= (65 + 26))
+//       strncat(temp, &temp, 1);
+//     if (temp == '(')
+//       stk->Push(temp);
+//     if ((temp == '*') || (temp == '/') || (temp == '+') || (temp == '-'))
+//       stk->Push(temp);
+//     if (temp == ')')
+//     {
+//       while (stk->Peek() != '(')
+//       {
+//          char temp2 = stk->Peek();
+//          strncat(tmp, &temp2, 1);
+//          stk->Pop();
+//       }
+//       stk->Pop();
+//     }
 
-    }
-}
+//     }
+// }
 
 void Calc::DisplayPostFix()
 {
